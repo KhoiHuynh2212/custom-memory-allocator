67fe98d82b5559d0c7ab8f27027ad35584df6815
KhoiHuynh2212 <khoihuynh2212@gmail.com>
2026-09-24 00:50:46 -0500

    Add struct medium chunk and redesign insert/unlink API into 1 dispatcher and 3 types of chunks, implememt malloc state in state.h

--- src/chunk.h
+++ src/chunk.h
@@ -96,132 +96,226 @@ typedef unsigned int flag_t;
 #define MALLOC_CHUNK_SIZE (sizeof(struct malloc_chunk))
 
 #if FOOTERS
-#define CHUNK_OVERHEAD      (sizeof(size_t) * 2)
+#define CHUNK_OVERHEAD (sizeof(size_t) * 2)
 #else /* FOOTERS */
-#define CHUNK_OVERHEAD      (sizeof(size_t))
+#define CHUNK_OVERHEAD (sizeof(size_t))
 #endif /* FOOTERS */
 
 /* MMapped chunks need a second word of overhead ... */
 #define MMAP_CHUNK_OVERHEAD (sizeof(size_t) * 2)
 /* ... and additional padding for fake next-chunk at foot */
-#define MMAP_FOOT_PAD       (sizeof(size_t) * 4)
+#define MMAP_FOOT_PAD (sizeof(size_t) * 4)
 
 /* The smallest size we can malloc is an aligned minimal chunk */
 #define MIN_CHUNK_SIZE ((MALLOC_CHUNK_SIZE + CHUNK_ALIGN_MASK) & ~CHUNK_ALIGN_MASK)
 
-static inline void* chunk_to_mem(void* p) {
-    return (void*) ((char*) p + sizeof(size_t) * 2);  /* point just right after the head*/
+static inline void *chunk_to_mem(void *p)
+{
+    return (void *)((char *)p + sizeof(size_t) * 2); /* point just right after the head*/
 }
 
-static inline struct malloc_chunk* mem_to_chunk(void* p) {
-    return (struct malloc_chunk *) ((char*) p - sizeof(size_t) * 2); /* go back to the pointer of the chunk */ 
-} 
+static inline struct malloc_chunk *mem_to_chunk(void *p)
+{
+    return (struct malloc_chunk *)((char *)p - sizeof(size_t) * 2); /* go back to the pointer of the chunk */
+}
 
-static inline struct malloc_chunk* align_as_chunk(void* p) {
-    return (struct malloc_chunk *) (p + align_offset(chunk_to_mem(p)));
+static inline struct malloc_chunk *align_as_chunk(void *p)
+{
+    return (struct malloc_chunk *)(p + align_offset(chunk_to_mem(p)));
 }
 
 /* Bounds on request (not chunk) sizes. */
-#define MAX_REQUEST         ((-MIN_CHUNK_SIZE) << 2)
-#define MIN_REQUEST         (MIN_CHUNK_SIZE - CHUNK_OVERHEAD - (size_t) 1) 
+#define MAX_REQUEST ((-MIN_CHUNK_SIZE) << 2)
+#define MIN_REQUEST (MIN_CHUNK_SIZE - CHUNK_OVERHEAD - (size_t)1)
 
 /* pad request bytes into a usable size */
-static inline size_t pad_request(size_t req) {
+static inline size_t pad_request(size_t req)
+{
     return (req + CHUNK_OVERHEAD + CHUNK_ALIGN_MASK) & ~CHUNK_ALIGN_MASK;
 }
 
 /* pad request, checking for minimum (but not maximum) */
-static inline size_t request_to_size(size_t req) {
+static inline size_t request_to_size(size_t req)
+{
     return req < MIN_REQUEST ? MIN_CHUNK_SIZE : pad_request(req);
 }
 
-static inline struct malloc_chunk *chunk_plus_offset(void *chunk, size_t size) {
-    return (struct malloc_chunk *) (((char *) chunk) + size);
+static inline struct malloc_chunk *chunk_plus_offset(void *chunk, size_t size)
+{
+    return (struct malloc_chunk *)(((char *)chunk) + size);
 }
 
-static inline struct malloc_chunk *chunk_minus_offset(void *chunk, size_t size) {
-    return (struct malloc_chunk *) (((char *) chunk) - size);
+static inline struct malloc_chunk *chunk_minus_offset(void *chunk, size_t size)
+{
+    return (struct malloc_chunk *)(((char *)chunk) - size);
 }
 
-static inline struct malloc_chunk *next_chunk(void *chunk) {
-    return (struct malloc_chunk *) (((char *) chunk) + (((struct any_chunk *) chunk)->head & ~FLAG_BITS));
+static inline struct malloc_chunk *next_chunk(void *chunk)
+{
+    return (struct malloc_chunk *)(((char *)chunk) + (((struct any_chunk *)chunk)->head & ~FLAG_BITS));
 }
 
-static inline struct malloc_chunk *prev_chunk(void *chunk) {
-    return (struct malloc_chunk *) (((char *) chunk) - (((struct any_chunk *) chunk)->prev_foot));
-} 
+static inline struct malloc_chunk *prev_chunk(void *chunk)
+{
+    return (struct malloc_chunk *)(((char *)chunk) - (((struct any_chunk *)chunk)->prev_foot));
+}
 
 /* Get the internal overhead associated with chunk p */
-static inline size_t overhead_for(void *chunk) {
+static inline size_t overhead_for(void *chunk)
+{
     return is_mmapped(chunk) ? MMAP_CHUNK_OVERHEAD : CHUNK_OVERHEAD;
 }
 
 /* Return true if malloced space is not necessarily cleared */
-static inline int calloc_must_clear(void *chunk) {
+static inline int calloc_must_clear(void *chunk)
+{
     return !is_mmapped(chunk);
-} 
+}
+/* macros to set up inuse chunks with or without footers */
+struct malloc_state;
 
+#if !FOOTERS
+
+static inline void mark_inuse_foot(struct malloc_state *state, void *chunk, size_t size)
+{
+    (void)state;
+    (void)chunk;
+    (void)size;
+}
 /* Set curr_inuse bit and prev_inuse bit of next chunk */
-static inline void set_inuse(struct malloc_state *state, void *chunk, size_t size) {
-    (void) state; // unused
-    ((struct any_chunk *) chunk)->head = (((struct any_chunk *) chunk)->head & PREV_INUSE_BIT) | size | CURR_INUSE_BIT;
-    ((struct malloc_chunk *) (((char *) chunk) + size))->head |= PREV_INUSE_BIT;
+static inline void set_inuse(struct malloc_state *state, void *chunk, size_t size)
+{
+    (void)state; // unused
+    ((struct any_chunk *)chunk)->head = (((struct any_chunk *)chunk)->head & PREV_INUSE_BIT) | size | CURR_INUSE_BIT;
+    ((struct malloc_chunk *)(((char *)chunk) + size))->head |= PREV_INUSE_BIT;
 }
 
 /* Set curr_inuse and prev_inuse of this chunk and prev_inuse of next chunk */
-static inline void set_inuse_and_prev_inuse(struct malloc_state *state, void *chunk, size_t size) {
-    (void) state; // unused
-    ((struct any_chunk *) chunk)->head = size | PREV_INUSE_BIT | CURR_INUSE_BIT;
-    ((struct malloc_chunk *) (((char *) chunk) + size))->head |= PREV_INUSE_BIT;
+static inline void set_inuse_and_prev_inuse(struct malloc_state *state, void *chunk, size_t size)
+{
+    (void)state; // unused
+    ((struct any_chunk *)chunk)->head = size | PREV_INUSE_BIT | CURR_INUSE_BIT;
+    ((struct malloc_chunk *)(((char *)chunk) + size))->head |= PREV_INUSE_BIT;
 }
 
 /* Set size, curr_inuse and prev_inuse bit of this chunk */
-static inline void set_size_and_prev_inuse_of_inuse_chunk(struct malloc_state *state, void *chunk, size_t size) {
-    (void) state; // unused
-    ((struct any_chunk *) chunk)->head = size | PREV_INUSE_BIT | CURR_INUSE_BIT;
+static inline void set_size_and_prev_inuse_of_inuse_chunk(struct malloc_state *state, void *chunk, size_t size)
+{
+    (void)state; // unused
+    ((struct any_chunk *)chunk)->head = size | PREV_INUSE_BIT | CURR_INUSE_BIT;
 }
 
-struct malloc_tree_chunk {
+#else /* FOOTERS */
+
+/* Set foot of inuse chunk to be xor of mstate and seed */
+#define mark_inuse_foot(M, p, s) \
+    (((struct malloc_chunk *)((char *)(p) + (s)))->prev_foot = ((size_t)(M) ^ params.magic))
+
+#define get_state_for(p)                                                \
+    ((struct malloc_state *)(((struct malloc_chunk *)((char *)(p) +     \
+                                                      (chunk_size(p)))) \
+                                 ->prev_foot ^                          \
+                             params.magic))
+
+#define set_inuse(M, p, s)                                                     \
+    ((p)->head = (((p)->head & PREV_INUSE_BIT) | s | CURR_INUSE_BIT),          \
+     (((struct malloc_chunk *)(((char *)(p)) + (s)))->head |= PREV_INUSE_BIT), \
+     mark_inuse_foot(M, p, s))
+
+#define set_inuse_and_prev_inuse(M, p, s)                                      \
+    ((p)->head = (s | PREV_INUSE_BIT | CURR_INUSE_BIT),                        \
+     (((struct malloc_chunk *)(((char *)(p)) + (s)))->head |= PREV_INUSE_BIT), \
+     mark_inuse_foot(M, p, s))
+
+#define set_size_and_prev_inuse_of_inuse_chunk(M, p, s) \
+    ((p)->head = (s | PREV_INUSE_BIT | CURR_INUSE_BIT), \
+     mark_inuse_foot(M, p, s))
+
+#endif /* !FOOTERS */
+
+struct malloc_tree_chunk
+{
     /* The first four fields must be compatible with malloc_chunk */
     size_t prev_foot;
     size_t head;
-    struct malloc_tree_chunk *fd;
-    struct malloc_tree_chunk *bk;
+    list list;
 
     struct malloc_tree_chunk *child[2];
     struct malloc_tree_chunk *parent;
     bin_index_t index;
-}; 
+};
 
 /*Helper macros for tree*/
-static inline struct malloc_tree_chunk* leftmost_child(struct malloc_tree_chunk * t) {
+static inline struct malloc_tree_chunk *leftmost_child(struct malloc_tree_chunk *t)
+{
     return t->child[0] != 0 ? t->child[0] : t->child[1];
-} 
-
-#define compute_tree_index(S, I)\
-{\
-  unsigned int X = S >> TREE_BIN_SHIFT;\
-  if (X == 0)\
-    I = 0;\
-  else if (X > 0xFFFF)\
-    I = NUM_TREE_BINS-1;\
-  else {\
-    unsigned int K = (unsigned) sizeof(X)*__CHAR_BIT__ - 1 - (unsigned) __builtin_clz(X); \
-    I =  (bin_index_t) ((K << 1) + ((S >> (K + (TREE_BIN_SHIFT-1)) & 1)));\
-  }\
 }
 
-void insert_chunk(struct malloc_state *, struct malloc_chunk *, size_t);
+#define compute_tree_index(S, I)                                                                  \
+    {                                                                                             \
+        unsigned int X = S >> TREE_BIN_SHIFT;                                                     \
+        if (X == 0)                                                                               \
+            I = 0;                                                                                \
+        else if (X > 0xFFFF)                                                                      \
+            I = NUM_TREE_BINS - 1;                                                                \
+        else                                                                                      \
+        {                                                                                         \
+            unsigned int K = (unsigned)sizeof(X) * __CHAR_BIT__ - 1 - (unsigned)__builtin_clz(X); \
+            I = (bin_index_t)((K << 1) + ((S >> (K + (TREE_BIN_SHIFT - 1)) & 1)));                \
+        }                                                                                         \
+    }
 
-void unlink_chunk(struct malloc_state *, struct malloc_chunk *, size_t); 
+struct malloc_medium_chunk
+{
+    size_t prev_foot;
+    size_t head;
 
-void insert_small_chunk(struct malloc_state *, struct malloc_chunk *, size_t);
+    list list; /* normal fd/bk bin links */
+    /* Only used for large blocks: pointer to next larger size.  */
+    struct malloc_medium_chunk *fd_nextsize; /* double links -- used only if free. */
+    struct malloc_medium_chunk *bk_nextsize;
+};
 
-void unlink_small_chunk(struct malloc_state *, struct malloc_chunk *, size_t);
+/* generic dispatcher */
+void insert_chunk(
+    struct malloc_state *state,
+    struct malloc_chunk *chunk,
+    size_t size);
 
-void unlink_first_small_chunk(struct malloc_state *, struct malloc_chunk *, struct malloc_chunk *, bin_index_t);
+void unlink_chunk(
+    struct malloc_state *state,
+    struct malloc_chunk *chunk,
+    size_t size);
 
-void replace_dv(struct malloc_state *, struct malloc_chunk *, size_t);
+/* small */
+void insert_small_chunk(
+    struct malloc_state *state,
+    struct malloc_chunk *chunk,
+    size_t size);
+
+void unlink_small_chunk(
+    struct malloc_state *state,
+    struct malloc_chunk *chunk,
+    size_t size);
+
+void unlink_first_small_chunk(
+    struct malloc_state *state,
+    struct malloc_chunk *bin,
+    struct malloc_chunk *chunk,
+    bin_index_t index);
+
+/* medium */
+void insert_medium_chunk(
+    struct malloc_state *state,
+    struct malloc_medium_chunk *chunk,
+    size_t size
+);
+
+void unlink_medium_chunk(
+    struct malloc_state *state,
+    struct malloc_medium_chunk *chunk,
+    size_t size
+);
 
 void insert_large_chunk(struct malloc_state *, struct malloc_tree_chunk *, size_t);
 
--- src/state.h
+++ src/state.h
@@ -1,4 +1,72 @@
-#ifdef MALLOC_STATE_H
+#ifndef MALLOC_STATE_H
 #define MALLOC_STATE_H
 
+#include "config.h"
+#include "chunk.h"
+
+/* Bin types, widths and sizes */
+#define NUM_SMALL_BINS  (32U)
+#define NUM_TREE_BINS   (32U)
+#define NUM_MEDIUM_BINS (32u)
+#define SMALL_BIN_SHIFT (3U)
+#define TREE_BIN_SHIFT  (8U)
+#define MIN_LARGE_SIZE ((size_t)1 << TREE_BIN_SHIFT)
+#define MAX_SMALL_SIZE (MIN_LARGE_SIZE - (size_t)1)
+#define MAX_SMALL_REQUEST (MAX_SMALL_SIZE - CHUNK_ALIGN_MASK - CHUNK_OVERHEAD)
+
+static inline int is_small(size_t size) {
+    return (size >> SMALL_BIN_SHIFT) < NUM_SMALL_BINS;
+}
+
+static inline bin_index_t small_index(size_t size) {
+    return (bin_index_t) (size >> SMALL_BIN_SHIFT);
+}
+
+static inline size_t small_index_to_size(bin_index_t index) {
+    return index << SMALL_BIN_SHIFT;
+}
+
+/* Shift placing maximum resolved bit in a tree_bin at i as sign bit */
+static inline bin_index_t leftshift_for_tree_index(bin_index_t i) {
+    return i == NUM_TREE_BINS - 1 ? 0 : SIZE_T_BITSIZE - (size_t) 1 - ((i >> 1) + TREE_BIN_SHIFT - 2);
+}
+
+/* The size of the smallest chunk held in bin with index i */
+static inline bin_index_t minsize_for_tree_index(bin_index_t i) {
+    return ((size_t) 1 << ((i >> 1) + TREE_BIN_SHIFT))
+           | (((size_t) (i & (size_t) 1)) << ((i >> 1) + TREE_BIN_SHIFT - 1));
+} 
+
+struct malloc_state
+{
+    /* global synchronization */
+     // mutex_t lock;
+
+    /* top chunk */
+    struct malloc_chunk *top;
+
+    /* unsorted staging */
+    struct malloc_chunk unsorted_bin;
+
+    /* small exact bins */
+    struct malloc_chunk small_bins[NUM_SMALL_BINS];
+
+    /* medium glibc-style bins */
+    struct malloc_medium_chunk medium_bins[NUM_MEDIUM_BINS];
+
+    /* large dlmalloc-style tree bins */
+    struct malloc_tree_chunk *tree_bins[NUM_TREE_BINS];
+
+    /* fast lookup for non-empty bins */
+    bin_map_t small_map;
+    bin_map_t medium_map;
+    bin_map_t tree_map;
+
+    /* memory/region accounting */
+    size_t footprint;
+    size_t max_footprint;
+
+    /* possibly region ownership later */
+    struct heap_region *regions;
+};
 #endif MALLOC_STATE_H
